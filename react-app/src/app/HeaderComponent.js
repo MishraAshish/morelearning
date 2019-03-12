2f4063e22d66af5da1fd4d2ce8c7b14899e984b4
Ashish <ash.ishu025g@gmail.com>
2019-03-12 23:18:30 +0530

    react first component

---
+++ react-app/src/app/HeaderComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = (props) =>{
+    return(
+        <div>
+            <h1>{ "This my own message for footer"}</h1>
+        </div>
+    )
+}
+
+export default Header = (props) =>{
+    return(
+        <div>
+            <h1>{"This my own message for header"}</h1>
+        </div>
+    )
+}
--- react-app/src/app/index.js
+++ react-app/src/app/index.js
@@ -1,1 +1,20 @@
 console.log("Test my application with --hot");
+import React, {Component, PureComponent} from "react";
+import Header, {Footer} from "./HeaderComponent";
+import {render} from "react-dom";
+
+class App extends Component{
+    constructor(){
+        super();        
+    }  
+    render(){
+        return(
+            <div>
+                <Header/>
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+render(<App/>, document.getElementById("app"))
